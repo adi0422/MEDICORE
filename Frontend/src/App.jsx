f0a7d0995c8c7c44c606f8d06ef5d2d0ab0dc2d4
Aditya Narayan Patra <115565670+adi0422@users.noreply.github.com>
2024-12-23 14:40:04 +0530

    NAVBAR designing

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctor from './pages/doctor'
+import Login from './pages/login'
+import About from './pages/about'
+import Contact from './pages/contact'
+import MyProfile from './pages/myprofile'
+import MyAppointment from './pages/myappointment'
+import Appointement from './pages/Appointement'
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route  path ="/" element={<Home/>} />
+        <Route  path ="/doctors" element={<Doctor/>} />
+        <Route  path ="/doctors/:speciality" element={<Doctor/>} />
+        <Route  path ="/login" element={<Login/>} />
+        <Route  path ="/about" element={<About/>} />
+        <Route  path ="/contact" element={<Contact/>} />
+        <Route  path ="/my-profile" element={<MyProfile/>} />
+        <Route  path ="/my-appointement" element={<MyAppointment/>} />
+        <Route  path ="/appointement/:docId" element={<Appointement/>} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Appointement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appointement = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Appointement
