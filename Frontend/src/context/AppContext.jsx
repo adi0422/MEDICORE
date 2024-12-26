6602c943a9ee6d0efd545e84e2b3f7f3da0592bb
Aditya Narayan Patra <115565670+adi0422@users.noreply.github.com>
2024-12-26 16:51:29 +0530

    docto

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -3,16 +3,14 @@ import { doctors } from "../assets/assets";
 
 export const AppContext = createContext()
 
-const AppContextProvider = (props) =>{
-
-
-    const value ={
-        doctors,
-    };
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
+const AppContextProvider = (props) => {
+    console.log(doctors); // Log the doctors data to debug
+    const value = { doctors };
+    return (
+      <AppContext.Provider value={value}>
+        {props.children}
+      </AppContext.Provider>
     );
-};
+  };
+   
 export default AppContextProvider;
